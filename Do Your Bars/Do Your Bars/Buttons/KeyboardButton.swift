5938c29f3d6db9e7a762d773610ee4def8d5ce77
Liliana Terry <lilianaterry@gmail.com>
2019-01-05 16:49:49 -0600

    fixed edit keyboard constraints

--- Do Your Bars/Do Your Bars/Buttons/KeyboardButton.swift
+++ Do Your Bars/Do Your Bars/Buttons/KeyboardButton.swift
@@ -26,9 +26,12 @@ class KeyboardButton: UIButton {
         commonInit()
     }
     
+    override func awakeFromNib() {
+        commonInit()
+    }
+    
     func commonInit() {
         layer.backgroundColor = UIColor.white.cgColor
-        layer.cornerRadius = frame.width / 2
         layer.applyShadow(color: toolKit.shadow, alpha: 0.16, x: 0, y: 3, blur: 16, spread: 0)
     }
 }
@@ -43,4 +46,57 @@ class CharKeyboardButton: KeyboardButton {
         ]
         return NSMutableAttributedString(string: (self.titleLabel?.text)!, attributes: attributes)
     }
+}
+
+class ColorKeyboardButton: KeyboardButton {
+    
+    var color: UIColor? { didSet { setNeedsDisplay() } }
+    var selectedLayer: CALayer?
+    var selectedPath: UIBezierPath?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSelectedLayer()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        addSelectedLayer()
+    }
+    
+    func addSelectedLayer() {
+        self.selectedLayer = CALayer()
+        self.layer.addSublayer(selectedLayer!)
+    }
+    
+    func addColor(color: UIColor) {
+        layer.backgroundColor = color.cgColor
+        self.color = color
+    }
+    
+    public func select() {
+        self.isSelected = true
+        setNeedsDisplay()
+    }
+    
+    public func deselect() {
+        self.isSelected = false
+        self.selectedPath = nil
+        setNeedsDisplay()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        if self.isSelected {
+            let center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
+            let radius = self.frame.width / 3
+            let width = self.frame.width / 8
+            
+            self.selectedPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+            
+            self.selectedPath?.lineWidth = width
+            UIColor.white.setStroke()
+            self.selectedPath?.stroke()
+        }
+    }
+    
 }
--- Do Your Bars/Do Your Bars/ViewControllers/EditViewController.swift
+++ Do Your Bars/Do Your Bars/ViewControllers/EditViewController.swift
@@ -13,41 +13,66 @@ class EditViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var keyboard: UIView!
     
+    @IBOutlet weak var redButton: ColorKeyboardButton!
+    @IBOutlet weak var blueButton: ColorKeyboardButton!
+    @IBOutlet weak var greenButton: ColorKeyboardButton!
+    @IBOutlet weak var blackButton: ColorKeyboardButton!
+    
     let toolKit = UIExtensions()
     
     var barText: NSAttributedString = NSAttributedString()
-    var currColor: UIColor = UIColor()
+    
+    var currColor: ColorKeyboardButton = ColorKeyboardButton()
+    
+    var colorButtons: Array<ColorKeyboardButton> = Array<ColorKeyboardButton>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupViews()
-        currColor = toolKit.black
+        setupKeyboard()
+        setupTextView()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        setupButtons()
-    }
-    
-    private func setupViews() {
+    private func setupKeyboard() {
         self.view.backgroundColor = toolKit.header_background
         keyboard.backgroundColor = toolKit.header_background
         
+        colorButtons = [redButton, blueButton, greenButton, blackButton]
+        redButton.addColor(color: toolKit.red)
+        blueButton.addColor(color: toolKit.blue)
+        greenButton.addColor(color: toolKit.green)
+        blackButton.addColor(color: toolKit.black)
+        
+        currColor = blackButton
+        currColor.select()
+        
         keyboard.removeFromSuperview()
-
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    // edit button corner radius after keyboard has appeared and constrained correctly
+    @objc private func keyboardWillAppear() {
+        let rowStack = keyboard.subviews[0]
+        let rows = rowStack.subviews
+        
+        for row in rows {
+            let buttons = row.subviews
+            for subview in buttons {
+                let button = subview as! KeyboardButton
+                button.layer.cornerRadius = button.frame.width / 2
+            }
+        }
+    }
+    
+    private func setupTextView() {
         let pinchRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(EditViewController.handleZoom(pinchRecognizer:)))
         textView.inputView = keyboard
         textView.addGestureRecognizer(pinchRecognizer)
         textView.attributedText = barText
-    }
-    
-    private func setupButtons() {
-        let innerStack = keyboard.subviews[0]
-        for subview in innerStack.subviews {
-            for button in subview.subviews {
-                button.layer.cornerRadius = button.frame.width / 2
-            }
-        }
     }
     
     @objc private func handleZoom(pinchRecognizer: UIPinchGestureRecognizer) {
@@ -63,7 +88,7 @@ class EditViewController: UIViewController {
     }
     
     @IBAction func addItem(_ sender: CharKeyboardButton) {
-        let newItem = sender.addItem(color: currColor)
+        let newItem = sender.addItem(color: currColor.color!)
         var currString = textView.attributedText.mutableCopy() as! NSMutableAttributedString
         let selection = textView.selectedRange
         
@@ -109,11 +134,30 @@ class EditViewController: UIViewController {
         textView.selectedRange = NSMakeRange(index + toolKit.space.length, 0)
     }
     
+    @IBAction func selectColor(_ sender: Any) {
+        let button = sender as! ColorKeyboardButton
+        
+        if (currColor != button) {
+            currColor.deselect()
+            currColor = button
+            currColor.select()
+        }
+    }
+    
+    private func updateBarCount() {
+        
+    }
+    
+    private func removeTrailingWhitespace() {
+        
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let navControl = segue.destination as! UINavigationController
         let destVC = navControl.topViewController as! ViewController
         destVC.barText = textView.attributedText
         destVC.currBarCount = 4
     }
+    
 
 }
